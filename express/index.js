f7973e193ea43f69949727d53ed0974611affc5b
Harsh bait <harshbait79@gmail.com>
2025-08-10 17:36:03 +0530

    Added code3 backend code folder

--- express/index.js
+++ express/index.js
@@ -12,29 +12,29 @@ app.use((req, res) =>{
     res.send('<h1>This Response using H1 Tag</h1> <ul> <li>Node</li> <li>Express</li></ul> ')
 })
 
-// app.get('/', (req, res) => {
-//     res.send("You contacted root path")
-// })
+app.get('/', (req, res) => {
+    res.send("You contacted root path")
+})
 
-// app.get('/home', (req, res) => {
-//     res.send("You contacted home path")
-// })
+app.get('/home', (req, res) => {
+    res.send("You contacted home path")
+})
 
-// app.get('/about', (req, res) => {
-//     res.send("You contacted about path")
-// })
+app.get('/about', (req, res) => {
+    res.send("You contacted about path")
+})
 
-// app.get('/search', (req, res)=>{
-//     console.log()
-//     let {q} = req.query
-//     res.send(`Send result is: ${q}`)
-// })
-// app.get('/:username', (req, res) => {
-//     let {username} = req.params;
-//     res.send(`Hello, I am ${username}`)
-// })
+app.get('/search', (req, res)=>{
+    console.log()
+    let {q} = req.query
+    res.send(`Send result is: ${q}`)
+})
+app.get('/:username', (req, res) => {
+    let {username} = req.params;
+    res.send(`Hello, I am ${username}`)
+})
 
 
-// app.all('*', (req, res) => {
-//     res.send("This path does not exist")
-// })
+app.all('*', (req, res) => {
+    res.send("This path does not exist")
+})
